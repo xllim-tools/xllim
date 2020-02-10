7c4e6a1537b010cc07cb871a9e94db3bf9f2a23c
SAMI DJOUADI <fs_djouadi@esi.dz>
2020-02-10 16:45:45 +0000

    Update launch.sh

--- test_env/launch.sh
+++ test_env/launch.sh
@@ -5,7 +5,7 @@ jupyter serverextension enable --py jupyterlab --sys-prefix
 mkdir -p root
 cd root
 
-cp ../kernel.cpython-37m-x86_64-linux-gnu.so ./kernel.cpython-37m-x86_64-linux-gnu.so
+cp ../kernelo.cpython-37m-x86_64-linux-gnu.so ./kernelo.cpython-37m-x86_64-linux-gnu.so
 cp ../test_hapke.json ./test_hapke.json
 cp ../test_script.ipynb ./test_script.ipynb
 
