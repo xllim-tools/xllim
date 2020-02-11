157e3fcdcc64dfecf6d4db9145c6b5c8e64c571e
SAMI DJOUADI <fs_djouadi@esi.dz>
2020-02-11 10:44:14 +0000

    Update launch.sh

--- test_env/launch.sh
+++ test_env/launch.sh
@@ -7,8 +7,7 @@ cd root
 
 ls /builds/fs_djouadi/kernelo/test_env
 
-curl --output kernelo.cpython-37m-x86_64-linux-gnu.so --header "PRIVATE-TOKEN: pxwCWfSgX95FVAkQ4USr" "https://gitlab.com/fs_djouadi/kernelo/-/jobs/artifacts/cicd_config/raw/kernelo.cpython-37m-x86_64-linux-gnu.so?job=build
-"
+curl --output kernelo.cpython-37m-x86_64-linux-gnu.so --location --output test.txt --header "PRIVATE-TOKEN:e1eoD8G9B8MmRMitas_y" "https://gitlab.com//api/v4/projects/16161938/jobs/artifacts/cicd_config/raw/kernelo.cpython-37m-x86_64-linux-gnu.so?job=build"
 
 cp ../test_hapke.json ./test_hapke.json
 cp ../test_script.ipynb ./test_script.ipynb
