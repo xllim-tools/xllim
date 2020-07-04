db84e806b65f1df14c0ef2158f91704fe1a12f0f
sami <fs_djouadi@esi.dz>
2020-07-04 23:31:30 +0100

    Fix : Rollback changes on the generated samples from the proposition law in the importance sampling module

--- src/importanceSampling/ImportanceSampler.cpp
+++ src/importanceSampling/ImportanceSampler.cpp
@@ -86,13 +86,13 @@ ISDiagnostic ImportanceSampler::diagnostic(
 
         // sample X_n
         samples.col(n) = isProposition->sample();
-        bool finish = false;
+        /*bool finish = false;
         do{
             samples.col(n) = isProposition->sample();
             if(samples.col(n).max() < 1 && samples.col(n).min() > 0){
                 finish = true;
             }
-        }while(!finish);
+        }while(!finish);*/
 
         // compute target density
         target_log_densities(n) = isTarget->target_log_density(samples.col(n), y_obs, y_cov);
