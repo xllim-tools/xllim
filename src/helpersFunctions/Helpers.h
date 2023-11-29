9ee7659daab601377648a0e0de837efe73e6be81
luc <luc.meyer@inria.fr>
2023-11-29 15:57:07 +0100

    cpptest & pytest basis for naive/fast inversion

--- src/helpersFunctions/Helpers.h
+++ src/helpersFunctions/Helpers.h
@@ -16,6 +16,7 @@ namespace Helpers{
     mat inverseMatrix(const mat& matrix);
     double weightedLogSumExp(const double & log_p1, const double & log_p2, const unsigned & c1, const unsigned & c2);
     vec dmvnrm_arma_fast_chol(mat const &x, rowvec const &mean, mat &sigma, bool const logd = true);
+    double mvnrm_arma_fast_chol(rowvec const &x, rowvec const &mean, mat &sigma, bool const logd = true);
     mat safe_cholesky(mat & Sigma);
     // arma::vec Mahalanobis(arma::mat const &x, arma::vec const &center, arma::mat const &cov);
 }
