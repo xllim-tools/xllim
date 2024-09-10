6e639b4923a6d4b2871d4bb4b0635adb8eeddba3
luc <luc.meyer@inria.fr>
2024-09-10 16:43:40 +0200

    rm test_env folder

--- test_env/jupyter_notebook_config.py
+++
@@ -1,8 +0,0 @@
-# https://devcenter.heroku.com/articles/ssl-endpoint
-#
-# As long as the notebook lives in the herokuapp.com domain,
-# we have SSL certificates enabled for encryption purposes.
-c.NotebookApp.ip = "*"
-c.NotebookApp.open_browser = False
-c.NotebookApp.password = u'sha1:a5c43a65be28:aeb69b90ca3d7395abf0c694d7ec680b3a2f29a7' #kernelo
-
