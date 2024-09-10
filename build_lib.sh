d8c67749eb9c4e43375a9d453f9301f948e75282
luc <luc.meyer@inria.fr>
2024-09-10 16:53:32 +0200

    rm/mv unwanted bash script files

--- build_lib.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-rm -rf dist/ build/ kernelo.egg-info/
-rm -rf cython/kernelo.cpp
-python3 setup.py sdist bdist_wheel -vvv
-pip install dist/kernelo-0.1-cp310-cp310-linux_x86_64.whl --force-reinstall
