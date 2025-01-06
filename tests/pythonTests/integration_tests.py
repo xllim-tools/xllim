741f2d7f30b611f91d2ce3522b696dafbb8d287f
luc <luc.meyer@inria.fr>
2025-01-06 12:42:32 +0100

    integration test update

--- tests/pythonTests/integration_tests.py
+++ tests/pythonTests/integration_tests.py
@@ -38,16 +38,32 @@ geometries_muk = {
 
 for geometries in [geometries_JSC1, geometries_muk]:
 
+    # Hapke 3p
+    variant = "2002"
+    adapter = "three"
+    theta_bar_scaling = 30.0
+    b0 = 1
+    h = 0
+
+    physical_models.append(
+        {
+            "name": "Hapke_3p_" + geometries["name"],
+            "xllim": xllim.HapkeModel(
+                geometries["data"], variant, adapter, theta_bar_scaling, b0, h
+            ),
+        }
+    )
+    
     # Hapke 4p
     variant = "2002"
     adapter = "four"
     theta_bar_scaling = 30.0
-    b0 = 0
-    h = 0.1
+    b0 = 1
+    h = 0
 
     physical_models.append(
         {
-            "name": "Hapke 4p " + geometries["name"],
+            "name": "Hapke_4p_" + geometries["name"],
             "xllim": xllim.HapkeModel(
                 geometries["data"], variant, adapter, theta_bar_scaling, b0, h
             ),
@@ -58,12 +74,12 @@ for geometries in [geometries_JSC1, geometries_muk]:
     variant = "2002"
     adapter = "six"
     theta_bar_scaling = 30.0
-    b0 = 0
-    h = 0.1
+    b0 = 1
+    h = 0
 
     physical_models.append(
         {
-            "name": "Hapke 6p " + geometries["name"],
+            "name": "Hapke_6p_" + geometries["name"],
             "xllim": xllim.HapkeModel(
                 geometries["data"], variant, adapter, theta_bar_scaling, b0, h
             ),
@@ -77,21 +93,21 @@ for geometries in [geometries_JSC1, geometries_muk]:
 
     physical_models.append(
         {
-            "name": "Shkuratov 5p " + geometries["name"],
+            "name": "Shkuratov_5p_" + geometries["name"],
             "xllim": xllim.ShkuratovModel(
                 geometries["data"], variant, scalingCoeffs, offset
             ),
         }
     )
 
-    # Schkuratov 5p
+    # Schkuratov 3p
     variant = "3p"
     scalingCoeffs = [1.0, 1.5, 1.5, 1.5, 1.5]
     offset = [0, 0, 0.2, 0, 0]
 
     physical_models.append(
         {
-            "name": "Shkuratov 3p " + geometries["name"],
+            "name": "Shkuratov_3p_" + geometries["name"],
             "xllim": xllim.ShkuratovModel(
                 geometries["data"], variant, scalingCoeffs, offset
             ),
@@ -149,18 +165,12 @@ for physical_model in physical_models:
         f.close()
 
     # compare results
-    print(physical_model["name"])
-    print("is_results")
-    print(is_results.predictions)
-    print("is_results_ref")
-    print(is_results_ref.predictions)
-    
-
-    assert np.allclose(is_results.predictions, is_results_ref.predictions)
-    assert np.allclose(is_results.predictions_variance, is_results_ref.predictions_variance)
-    assert np.allclose(is_results.nb_effective_sample, is_results_ref.nb_effective_sample)
-    assert np.allclose(is_results.effective_sample_size, is_results_ref.effective_sample_size)
-    assert np.allclose(is_results.qn, is_results_ref.qn)
+    error_msg = "importanceSampling" + " > " + physical_model["name"]
+    assert np.allclose(is_results.predictions, is_results_ref.predictions), error_msg + " > " + "predictions"
+    assert np.allclose(is_results.predictions_variance, is_results_ref.predictions_variance), error_msg + " > " + "predictions_variance"
+    assert np.allclose(is_results.nb_effective_sample, is_results_ref.nb_effective_sample), error_msg + " > " + "nb_effective_sample"
+    assert np.allclose(is_results.effective_sample_size, is_results_ref.effective_sample_size), error_msg + " > " + "effective_sample_size"
+    assert np.allclose(is_results.qn, is_results_ref.qn), error_msg + " > " + "qn"
 
 
 # ? ###########################################################################
@@ -183,7 +193,7 @@ train_ratio_ll = -1000  # Force train to reach max iteration
 train_floor = 1e-12
 K_merged = 2
 
-covariance_type_list = ["Full", "Diag", "Iso"]
+covariance_type_list = ["full", "diag", "iso"]
 K, D, L, N = 5, 9, 4, 100
 
 for gamma_type in covariance_type_list:
@@ -196,7 +206,7 @@ for gamma_type in covariance_type_list:
         #####################  Set up specific GLLiM model  #######################
 
         gllim = xllim.GLLiM(
-            K, D, L, gamma_type.lower(), sigma_type.lower(), 0
+            K, D, L, gamma_type, sigma_type, 0
         )  # hidden_values = 0
 
         # ! #######################  TEST : initialize()  #########################
@@ -211,7 +221,7 @@ for gamma_type in covariance_type_list:
             gmm_floor,
             nb_experiences,
             seed,
-            1,
+            0,
         )
         gllim_params_initialised = gllim.getParams()
 
@@ -225,23 +235,24 @@ for gamma_type in covariance_type_list:
             f.close()
 
         # compare results
-        assert np.allclose(gllim_params_initialised.Pi, gllim_params_initialised_ref.Pi)
-        assert np.allclose(gllim_params_initialised.A, gllim_params_initialised_ref.A)
-        assert np.allclose(gllim_params_initialised.B, gllim_params_initialised_ref.B)
-        assert np.allclose(gllim_params_initialised.C, gllim_params_initialised_ref.C)
-        assert np.allclose(gllim_params_initialised.Gamma, gllim_params_initialised_ref.Gamma)
-        assert np.allclose(gllim_params_initialised.Sigma, gllim_params_initialised_ref.Sigma)
+        error_msg = "initialize" + " > " + gamma_type + "/" + sigma_type
+        assert np.allclose(gllim_params_initialised.Pi, gllim_params_initialised_ref.Pi), error_msg + " > " + "Pi"
+        assert np.allclose(gllim_params_initialised.A, gllim_params_initialised_ref.A), error_msg + " > " + "A"
+        assert np.allclose(gllim_params_initialised.B, gllim_params_initialised_ref.B), error_msg + " > " + "B"
+        assert np.allclose(gllim_params_initialised.C, gllim_params_initialised_ref.C), error_msg + " > " + "C"
+        assert np.allclose(gllim_params_initialised.Gamma, gllim_params_initialised_ref.Gamma), error_msg + " > " + "Gamma"
+        assert np.allclose(gllim_params_initialised.Sigma, gllim_params_initialised_ref.Sigma), error_msg + " > " + "Sigma"
 
         # ! #########################  TEST : train()  ############################
 
-        if gamma_type == "Full" and sigma_type == "Full":
+        if gamma_type == "full" and sigma_type == "full":
             gllim.train(
                 np.array(x_gen_random.T),
                 np.array(y_gen_random.T),
                 gmm_kmeans_iteration,
                 train_ratio_ll,
                 train_floor,
-                1,
+                0,
             )
         else:
             gllim.train(
@@ -250,7 +261,7 @@ for gamma_type in covariance_type_list:
                 train_max_iteration,
                 train_ratio_ll,
                 train_floor,
-                1,
+                0,
             )
         gllim_params_trained = gllim.getParams()
 
@@ -264,42 +275,44 @@ for gamma_type in covariance_type_list:
             f.close()
 
         # compare results
-        assert np.allclose(gllim_params_trained.Pi, gllim_params_trained_ref.Pi)
-        assert np.allclose(gllim_params_trained.A, gllim_params_trained_ref.A)
-        assert np.allclose(gllim_params_trained.B, gllim_params_trained_ref.B)
-        assert np.allclose(gllim_params_trained.C, gllim_params_trained_ref.C)
-        assert np.allclose(gllim_params_trained.Gamma, gllim_params_trained_ref.Gamma)
-        assert np.allclose(gllim_params_trained.Sigma, gllim_params_trained_ref.Sigma)
+        error_msg = "train" + " > " + gamma_type + "/" + sigma_type
+        assert np.allclose(gllim_params_initialised.Pi, gllim_params_initialised_ref.Pi), error_msg + " > " + "Pi"
+        assert np.allclose(gllim_params_initialised.A, gllim_params_initialised_ref.A), error_msg + " > " + "A"
+        assert np.allclose(gllim_params_initialised.B, gllim_params_initialised_ref.B), error_msg + " > " + "B"
+        assert np.allclose(gllim_params_initialised.C, gllim_params_initialised_ref.C), error_msg + " > " + "C"
+        assert np.allclose(gllim_params_initialised.Gamma, gllim_params_initialised_ref.Gamma), error_msg + " > " + "Gamma"
+        assert np.allclose(gllim_params_initialised.Sigma, gllim_params_initialised_ref.Sigma), error_msg + " > " + "Sigma"
 
 
-        # ! ####################  TEST : inverseDensities()  ######################
+        # # ! ####################  TEST : inverseDensities()  ######################
 
-        prediction_floor = 1e-10
+        # prediction_floor = 1e-10
 
-        prediction_results = gllim.inverseDensities(
-            np.array(y_gen_random.T),
-            np.zeros(D),
-            K_merged,
-            prediction_floor,
-        )  # vectorized
+        # prediction_results = gllim.inverseDensities(
+        #     np.array(y_gen_random.T),
+        #     np.zeros(D),
+        #     K_merged,
+        #     prediction_floor,
+        # )  # vectorized
 
-        # ! Only run once to generate ref
-        with open("../dataRef/prediction_results_ref/prediction_results_ref_{}_{}.file".format(gamma_type, sigma_type), "wb") as f:
-            pickle.dump(prediction_results, f)
-            f.close()
+        # # ! Only run once to generate ref
+        # with open("../dataRef/prediction_results_ref/prediction_results_ref_{}_{}.file".format(gamma_type, sigma_type), "wb") as f:
+        #     pickle.dump(prediction_results, f)
+        #     f.close()
 
-        with open("../dataRef/prediction_results_ref/prediction_results_ref_{}_{}.file".format(gamma_type, sigma_type), "rb") as f:
-            prediction_results_ref = pickle.load(f)
-            f.close()
+        # with open("../dataRef/prediction_results_ref/prediction_results_ref_{}_{}.file".format(gamma_type, sigma_type), "rb") as f:
+        #     prediction_results_ref = pickle.load(f)
+        #     f.close()
 
-        # compare results
-        assert np.allclose(prediction_results.fullGMM.weights, prediction_results_ref.fullGMM.weights)
-        assert np.allclose(prediction_results.fullGMM.means, prediction_results_ref.fullGMM.means)
-        assert np.allclose(prediction_results.fullGMM.covs, prediction_results_ref.fullGMM.covs)
-        assert np.allclose(prediction_results.fullGMM.mean, prediction_results_ref.fullGMM.mean)
-        assert np.allclose(prediction_results.fullGMM.variance, prediction_results_ref.fullGMM.variance)
-        assert np.allclose(prediction_results.mergedGMM.weights, prediction_results_ref.mergedGMM.weights)
-        assert np.allclose(prediction_results.mergedGMM.means, prediction_results_ref.mergedGMM.means)
-        assert np.allclose(prediction_results.mergedGMM.covs, prediction_results_ref.mergedGMM.covs)
-        assert np.allclose(prediction_results.mergedGMM.mean, prediction_results_ref.mergedGMM.mean)
-        assert np.allclose(prediction_results.mergedGMM.variance, prediction_results_ref.mergedGMM.variance)
+        # # compare results
+        # error_msg = "inverseDensities" + " > " + gamma_type + "/" + sigma_type
+        # assert np.allclose(prediction_results.fullGMM.weights, prediction_results_ref.fullGMM.weights), error_msg + " > " + "fullGMM.weights"
+        # assert np.allclose(prediction_results.fullGMM.means, prediction_results_ref.fullGMM.means), error_msg + " > " + "fullGMM.means"
+        # assert np.allclose(prediction_results.fullGMM.covs, prediction_results_ref.fullGMM.covs), error_msg + " > " + "fullGMM.covs"
+        # assert np.allclose(prediction_results.fullGMM.mean, prediction_results_ref.fullGMM.mean), error_msg + " > " + "fullGMM.mean"
+        # assert np.allclose(prediction_results.fullGMM.variance, prediction_results_ref.fullGMM.variance), error_msg + " > " + "fullGMM.variance"
+        # assert np.allclose(prediction_results.mergedGMM.weights, prediction_results_ref.mergedGMM.weights), error_msg + " > " + "mergedGMM.weights"
+        # assert np.allclose(prediction_results.mergedGMM.means, prediction_results_ref.mergedGMM.means), error_msg + " > " + "mergedGMM.means"
+        # assert np.allclose(prediction_results.mergedGMM.covs, prediction_results_ref.mergedGMM.covs), error_msg + " > " + "mergedGMM.covs"
+        # assert np.allclose(prediction_results.mergedGMM.mean, prediction_results_ref.mergedGMM.mean), error_msg + " > " + "mergedGMM.mean"
+        # assert np.allclose(prediction_results.mergedGMM.variance, prediction_results_ref.mergedGMM.variance), error_msg + " > " + "mergedGMM.variance"
