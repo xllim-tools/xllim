9c90eccd5661fb26a6b41b059a5fc56748953fb1
luc <luc.meyer@inria.fr>
2025-02-10 17:58:51 +0100

    upgrade script for building and testing within docker container

--- build_lib_docker.sh
+++ build_lib_docker.sh
@@ -1,17 +1,43 @@
 #!/bin/bash
 
-# 1st argument = copy directory path = $1
-# 2nd argument = backup directory path = $2
+# --image option = Docker image name with tag in which the tests will be runned
+# --lib-dir option = path to directory where the xllim shared linrary .so will be installed
+# --backup-dir option = path to directory in which a backup of the library will be saved with date and time.
 
-echo "\033[35m \n-> Remove previous xllim libraries \033[0m"
-rm -rf $1/xllim*.so -v
-rm -rf *.so -v
 
-echo "\033[35m \n-> Run the builder docker container bounded to xllim app \033[0m"
-docker run -it -d --rm --name xllim_builder_temp_container -v $(pwd):/home xllim_builder:jammy
+# Parse options
+ARGS=$(getopt -o "" --long image:,lib-dir:,backup-dir: -- "$@")
+eval set -- "$ARGS"
 
-# Idée pour artefact CI : faire sortir la library .so dans .
-# on a bien build/ qui est modifié dans host donc pourquoi pas la lib ?
+# Default values
+image=""
+lib_dir=""
+backup_dir=""
+
+# Read the arguments
+while true; do
+  case "$1" in
+    --image) image="$2"; shift 2 ;;
+    --lib-dir) lib_dir="$2"; shift 2 ;;
+    --backup-dir) backup_dir="$2"; shift 2 ;;
+    --) shift; break ;;  # End of options
+    *) break ;;
+  esac
+done
+
+# Ensure paths DO NOT end with '/'
+if [ -n "$lib_dir" ] && [ "${lib_dir%/}" != "$lib_dir" ]; then
+  lib_dir="${lib_dir%/}"
+fi
+if [ -n "$backup_dir" ] && [ "${backup_dir%/}" != "$backup_dir" ]; then
+  backup_dir="${backup_dir%/}"
+fi
+
+
+
+echo "\033[35m \n-> Run the builder docker container bounded to xllim app : $image \033[0m"
+docker run -it -d --rm --name xllim_builder_temp_container -v $(pwd):/home $image
+
 
 echo "\033[35m \n-> Build xllim in docker container \033[0m"
 docker exec -i xllim_builder_temp_container bash -c "\
@@ -19,19 +45,18 @@ docker exec -i xllim_builder_temp_container bash -c "\
     cmake -S .. -B . -DPYTHON_LIBRARY_DIR="/usr/lib/python3/dist-packages" -DCMAKE_BUILD_TYPE=Release && \
     make install "
 
-echo "\033[35m \n-> Copy xllim library to host\033[0m"
-docker cp xllim_builder_temp_container:/usr/lib/python3/dist-packages/xllim.cpython-310-x86_64-linux-gnu.so .
-# docker cp xllim_builder_temp_container:/usr/lib/python3/dist-packages/xllim.cpython-312-x86_64-linux-gnu.so .
 
-echo "\033[35m \n-> Rename library and move it to python host site-packages ($1) and back-up ($2) directories \033[0m"
-file=$(ls *.so)
-cp $file $1/xllim.so -v
-cp $file $2/${file%.so}.$(date +%F_%T).so -v
+echo "\033[35m \n-> Copy xllim library to host python site-packages at $lib_dir \033[0m"
+file="xllim.cpython-310-x86_64-linux-gnu.so"
+# file="xllim.cpython-312-x86_64-linux-gnu.so" # depending on python version
+docker cp xllim_builder_temp_container:/usr/lib/python3/dist-packages/file $lib_dir
 
-rm -rf *.so -v
 
-BACK_UP_LIBS=$(ls $2 | wc -l)
-echo "\033[33m There are ($BACK_UP_LIBS) back-up xllim libraries in ($2) \033[0m"
+echo "\033[35m \n-> Rename library and move it to back-up ($backup_dir) directories \033[0m"
+cp $lib_dir/$file $backup_dir/${file%.so}.$(date +%F_%T).so -v
+BACK_UP_LIBS=$(ls $backup_dir | wc -l)
+echo "\033[33m There are ($BACK_UP_LIBS) back-up xllim libraries in ($backup_dir) \033[0m"
+
 
 echo "\033[35m \n-> Stop the docker container \033[0m"
 docker stop xllim_builder_temp_container
--- test_lib_docker.sh
+++ test_lib_docker.sh
@@ -1,21 +1,37 @@
 #!/bin/bash
 
-# 1st argument = copy directory path = $1
-# 2nd argument = backup directory path = $2
+# --image option = Docker image name with tag in which the tests will be runned
+# --lib-path option = path to the xllim shared linrary .so used for the Python tests
 
-echo "\033[35m \n-> Run the builder docker container bounded to xllim app \033[0m"
-docker run -it -d --rm --name xllim_builder_temp_container -v $(pwd):/home xllim_builder:jammy
+# Parse options
+ARGS=$(getopt -o "" --long image:,lib-path: -- "$@")
+eval set -- "$ARGS"
+
+# Default values
+image=""
+lib_path=""
+
+# Read the arguments
+while true; do
+  case "$1" in
+    --image) image="$2"; shift 2 ;;
+    --lib-path) lib_path="$2"; shift 2 ;;
+    --) shift; break ;;  # End of options
+    *) break ;;
+  esac
+done
+
+
+echo "\033[35m \n-> Run the builder docker container bounded to xllim app : $image \033[0m"
+docker run -it -d --rm --name xllim_builder_temp_container -v $(pwd):/home $image
 
 
 echo "\033[35m \n-> CPP unit tests \033[0m"
 docker exec -i xllim_builder_temp_container bash -c "cd /home/build && ctest"
 # docker exec -i xllim_builder_temp_container bash -c "cd /home/build && ctest -V -R TestModelTest.GetLDimension"
 
-echo "\033[35m \n-> Python integration tests \033[0m"
-docker cp $1/xllim.so xllim_builder_temp_container:/usr/lib/python3/dist-packages/xllim.so
-# docker exec -i xllim_builder_temp_container bash -c "cd /home/tests/pythonTests && python3 gllim_script.py"
-# docker exec -i xllim_builder_temp_container bash -c "cd /home/tests/pythonTests && python3 functional_model_script.py"
-# docker exec -i xllim_builder_temp_container bash -c "cd /home/tests/pythonTests && /usr/bin/python3 integration_tests.py"
+echo "\033[35m \n-> Python integration tests with the xllim compiled lib at $lib_path \033[0m"
+docker cp $lib_path xllim_builder_temp_container:/usr/lib/python3/dist-packages/xllim.so
 docker exec -i xllim_builder_temp_container bash -c "cd /home/tests/pythonTests && pytest -v --tb=short --color=yes" # -s for stdout and prints
 
 echo "\033[35m \n-> Stop the docker container \033[0m"
